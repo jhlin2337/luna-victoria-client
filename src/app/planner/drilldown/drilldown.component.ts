162459ed3d741d52824aa187ffee5958f660379f
jhlin2337 <James@Jamess-MacBook-Air.local>
2018-04-15 17:16:38 -0700

    Drilldown now parses date from url

--- src/app/planner/drilldown/drilldown.component.ts
+++ src/app/planner/drilldown/drilldown.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-drilldown',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrilldownComponent implements OnInit {
 
-  constructor() { }
+  date: Date;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe((params) => {
+        this.date = new Date(Number(params.date));
+      });
   }
 
 }
