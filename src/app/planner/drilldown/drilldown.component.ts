b409aa7f4d88f14835b5c4b3fda52b64dd54baac
jhlin2337 <James@Jamess-MacBook-Air.local>
2018-04-15 22:01:43 -0700

    Drilldown now displays actual data

--- src/app/planner/drilldown/drilldown.component.ts
+++ src/app/planner/drilldown/drilldown.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GoalService } from '../goal.service';
 
 @Component({
   selector: 'app-drilldown',
@@ -9,14 +10,42 @@ import { ActivatedRoute } from '@angular/router';
 export class DrilldownComponent implements OnInit {
 
   date: Date;
+  currMonthGoals: [any];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  getSub;
+
+  constructor(private activatedRoute: ActivatedRoute, private goalService: GoalService) { }
 
   ngOnInit() {
+    this.getParams();
+    this.getCurrMonthGoals();
+  }
+
+  getParams() {
     this.activatedRoute.params
       .subscribe((params) => {
         this.date = new Date(Number(params.date));
       });
   }
 
+  getCurrMonthGoals() {
+    if (this.getSub) {
+      this.getSub.unsubscribe();
+    }
+
+    // Get the epoch time for the current month
+    const currMonthDate = this.date;
+    const currMonthEpoch = currMonthDate.getTime();
+
+    // Get the epoch time for next month
+    const nextMonthDate = currMonthDate;
+    nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
+    const nextMonthEpoch = nextMonthDate.getTime();
+
+    // Get goals that are due this month
+    this.getSub = this.goalService.getGoals(currMonthEpoch, nextMonthEpoch)
+      .subscribe((data: any) => {
+        this.currMonthGoals = data;
+      });
+  }
 }
