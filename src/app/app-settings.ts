fcf5a7c7d60c9f51f5698745bb885da37dc665fb
jhlin2337 <James@Jamess-MacBook-Air.local>
2018-04-17 17:19:05 -0700

    Changed api endpoint

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,4 +1,5 @@
 export class AppSettings {
-  public static API_ENDPOINT = 'https://luna-victoria-server.herokuapp.com/api/';
+  // public static API_ENDPOINT = 'https://luna-victoria-server.herokuapp.com/api/';
+  public static API_ENDPOINT = 'https://lunavictoria.herokuapp.com/api/';
   public static JWT_TOKEN = 'JWT_TOKEN';
 }
